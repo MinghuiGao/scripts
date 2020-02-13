ebba1f3b98663bc76e1061dcf840eadf6da24ef5
MinghuiGao <minghui.gao@ninebot.com>
2020-02-14 01:38:20 +0800

    remove chinese info

--- ubuntu_install_v2ray.sh
+++ ubuntu_install_v2ray.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# v2ray Ubuntu系统一键安装脚本
+# v2ray Ubuntu scripts
 # Author: hijk<https://www.hijk.pw>
 
 echo "#############################################################"
 echo "#         Ubuntu 16.04 TLS v2ray 带一键安装脚本              #"
-echo "# 网址: https://www.hijk.pw                                 #"
-echo "# 作者: hijk                                                #"
+echo "# address: https://www.hijk.pw                                 #"
+echo "# author: hijk                                                #"
 echo "#############################################################"
 echo ""
 
@@ -17,20 +17,20 @@ function checkSystem()
 {
     result=$(id | awk '{print $1}')
     if [ $result != "uid=0(root)" ]; then
-        echo "请以root身份执行该脚本"
+        echo "please use root run this script."
         exit 1
     fi
 
     res=`lsb_release -d | grep -i ubuntu`
     if [ "${res}" = "" ];then
-        echo "系统不是Ubuntu"
+        echo "system is not Ubuntu"
         exit 1
     fi
-    
+
     result=`lsb_release -d | grep -oE "[0-9.]+"`
     main=${result%%.*}
     if [ $main -lt 16 ]; then
-        echo "不受支持的Ubuntu版本"
+        echo "not supported Ubuntu version"
         exit 1
     fi
 }
@@ -39,20 +39,20 @@ function getData()
 {
     while true
     do
-        read -p "请输入v2ray的端口[1-65535]:" port
+        read -p "enter v2ray port [1-65535]:" port
         [ -z "$port" ] && port="21568"
         expr $port + 0 &>/dev/null
         if [ $? -eq 0 ]; then
             if [ $port -ge 1 ] && [ $port -le 65535 ]; then
                 echo ""
-                echo "端口号： $port"
+                echo "port is ： $port"
                 echo ""
                 break
             else
-                echo "输入错误，端口号为1-65535的数字"
+                echo "wrong input ,should between 1-65535"
             fi
         else
-            echo "输入错误，端口号为1-65535的数字"
+            echo "wrong input ,should between 1-65535"
         fi
     done
 }
@@ -63,10 +63,10 @@ function preinstall()
     systemctl restart sshd
     ret=`nginx -t`
     if [ "$?" != "0" ]; then
-        echo "更新系统..."
+        echo "system updating..."
         apt update && apt -y upgrade
     fi
-    echo "安装必要软件"
+    echo "install nessary softwear."
     apt install -y telnet wget vim net-tools ntpdate unzip
     apt autoremove -y
 }
@@ -77,7 +77,7 @@ function installV2ray()
     bash <(curl -L -s https://install.direct/go.sh)
 
     if [ ! -f /etc/v2ray/config.json ]; then
-        echo "安装失败，请到 https://www.hijk.pw 网站反馈"
+        echo "install failed. visit https://www.hijk.pw and feedback"
         exit 1
     fi
 
@@ -95,10 +95,10 @@ function installV2ray()
     sleep 3
     res=`netstat -nltp | grep ${port} | grep v2ray`
     if [ "${res}" = "" ]; then
-        echo "v2ray启动失败，请检查端口是否被占用！"
+        echo "v2ray starting failed , check port is ocupied ot not."
         exit 1
     fi
-    echo "v2ray安装成功！"
+    echo "v2ray install success."
 }
 
 function setFirewall()
@@ -114,14 +114,14 @@ function installBBR()
 {
     result=$(lsmod | grep bbr)
     if [ "$result" != "" ]; then
-        echo BBR模块已安装
+        echo BBR model already installed.
         bbr=true
         echo "3" > /proc/sys/net/ipv4/tcp_fastopen
         echo "net.ipv4.tcp_fastopen = 3" >> /etc/sysctl.conf
         return;
     fi
 
-    echo 安装BBR模块...
+    echo installing BBR model ...
     apt install -y --install-recommends linux-generic-hwe-16.04
     grub-set-default 0
     echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
@@ -137,35 +137,35 @@ function info()
     ip=`curl -s -4 icanhazip.com`
     port=`cat /etc/v2ray/config.json | grep port | cut -d: -f2 | tr -d \",' '`
     res=`netstat -nltp | grep ${port} | grep v2ray`
-    [ -z "$res" ] && status="${red}已停止${plain}" || status="${green}正在运行${plain}"
+    [ -z "$res" ] && status="${red} stopped ${plain}" || status="${green} is running ${plain}"
     uid=`cat /etc/v2ray/config.json | grep id | cut -d: -f2 | tr -d \",' '`
     alterid=`cat /etc/v2ray/config.json | grep alterId | cut -d: -f2 | tr -d \",' '`
     res=`cat /etc/v2ray/config.json | grep network`
     [ -z "$res" ] && network="tcp" || network=`cat /etc/v2ray/config.json | grep network | cut -d: -f2 | tr -d \",' '`
     security="auto"
-    
+
     echo ============================================
-    echo -e " v2ray运行状态：${status}"
-    echo -e " v2ray配置文件：${red}/etc/v2ray/config.json${plain}"
+    echo -e " v2ray running status：${status}"
+    echo -e " v2ray config file：${red}/etc/v2ray/config.json${plain}"
     echo ""
-    echo -e "${red}v2ray配置信息：${plain}               "
+    echo -e "${red}v2ray config info：${plain}               "
     echo -e " IP(address):  ${red}${ip}${plain}"
-    echo -e " 端口(port)：${red}${port}${plain}"
+    echo -e " port (port)：${red}${port}${plain}"
     echo -e " id(uuid)：${red}${uid}${plain}"
-    echo -e " 额外id(alterid)： ${red}${alterid}${plain}"
-    echo -e " 加密方式(security)： ${red}$security${plain}"
-    echo -e " 传输协议(network)： ${red}${network}${plain}" 
-    echo  
+    echo -e " extra id(alterid)： ${red}${alterid}${plain}"
+    echo -e " encryption (security)： ${red}$security${plain}"
+    echo -e " transport protocal (network)： ${red}${network}${plain}"
+    echo
     echo ============================================
 }
 
 function bbrReboot()
 {
     if [ "${bbr}" == "false" ]; then
-        echo  
-        echo  为使BBR模块生效，系统将在30秒后重启
-        echo  
-        echo -e "您可以按 ctrl + c 取消重启，稍后输入 ${red}reboot${plain} 重启系统"
+        echo
+        echo  in order to make BBR work，system will reboot in 30s.
+        echo
+        echo -e "use ctrl + c to cancle reboot，and input ${red}reboot${plain} to reboot system later."
         sleep 30
         reboot
     fi
@@ -174,7 +174,7 @@ function bbrReboot()
 
 function install()
 {
-    echo -n "系统版本:  "
+    echo -n "System Version:  "
     lsb_release -a
 
     checkSystem
@@ -183,14 +183,14 @@ function install()
     installBBR
     installV2ray
     setFirewall
-    
+
     info
     bbrReboot
 }
 
 function uninstall()
 {
-    read -p "您确定真的要卸载v2ray吗？(y/n)" answer
+    read -p "are you sure to uninstall v2ray？(y/n)" answer
     [ -z ${answer} ] && answer="n"
 
     if [ "${answer}" == "y" ] || [ "${answer}" == "Y" ]; then
@@ -200,8 +200,8 @@ function uninstall()
         rm -rf /usr/bin/v2ray/*
         rm -rf /var/log/v2ray/*
         rm -rf /etc/systemd/system/v2ray.service
-        
-        echo -e " ${red}卸载成功${plain}"
+
+        echo -e " ${red}uninstall success ${plain}"
     fi
 }
 
@@ -212,7 +212,7 @@ case "$action" in
         ${action}
         ;;
     *)
-        echo "参数错误"
-        echo "用法: `basename $0` [install|uninstall]"
+        echo "wrong parameters."
+        echo "example: `basename $0` [install|uninstall]"
         ;;
 esac
